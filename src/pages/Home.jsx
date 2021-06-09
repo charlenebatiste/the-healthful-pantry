919a55f4d064898142f9b4df941c6144a78c3a75
charlenebatiste <batistecharlene@gmail.com>
2021-06-09 11:57:10 -0400

    created pages folder and updated organization

---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import About from "../components/About";
+
+function Home(props) {
+	return (
+		<div>
+			<h2>Welcome to the Homepage</h2>
+			<About />
+		</div>
+	);
+}
+export default Home;
