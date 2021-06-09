b0365f05ee6732336502b918095e3a9d602aa25c
charlenebatiste <batistecharlene@gmail.com>
2021-06-09 15:54:05 -0400

    edit hotitworks

--- src/components/HowItWorks.jsx
+++ src/components/HowItWorks.jsx
@@ -1,12 +1,11 @@
-import React from 'react';
+import React from "react";
 
 const HowItWorks = () => {
-    return (
-        <div>
-            <h1>HowItWorks</h1>
-
-        </div >
-    )
-}
+	return (
+		<div>
+			<h1>HowItWorks</h1>
+		</div>
+	);
+};
 
 export default HowItWorks;
