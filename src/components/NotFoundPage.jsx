dadf821b99c86202ed0b14d7d8b77684d26f5609
charlenebatiste <batistecharlene@gmail.com>
2021-06-09 18:06:39 -0400

    update input fields and added const values

--- src/components/NotFoundPage.jsx
+++ src/components/NotFoundPage.jsx
@@ -1,18 +1,38 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import PageNotFound from '../img/PageNotFound.jpg';
-import './NotFoundPage.css'
+import React from "react";
+import { Link } from "react-router-dom";
+import PageNotFound from "../img/PageNotFound.jpg";
+import "./NotFoundPage.css";
 
-class NotFoundPage extends React.Component{
-    render(){
-        return <div className="container my-4">
-            <img className="img-responsive" width="304" height="236" src={PageNotFound}  />
-            <div className="text-center"> <h1>Sorry!</h1> <br/>
-            <p>This page isn't working</p> <br/> 
-            <p style={{textAlign:"center"}}>
-              <Link to="/">Go to Home </Link>
-            </p></div>
-          </div>;
-    }
+class NotFoundPage extends React.Component {
+	render() {
+		return (
+			<div className="container my-4">
+				<img
+					className="img-responsive"
+					width="304"
+					height="236"
+					src={PageNotFound}
+					alt="shocked little girl"
+				/>
+				<div className="text-center">
+					{" "}
+					<h1>Sorry!</h1> <br />
+					<p>
+						This page isn't working
+					</p>{" "}
+					<br />
+					<p
+						style={{
+							textAlign: "center",
+						}}
+					>
+						<Link to="/">
+							Go to Home{" "}
+						</Link>
+					</p>
+				</div>
+			</div>
+		);
+	}
 }
 export default NotFoundPage;
