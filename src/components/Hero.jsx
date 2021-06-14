60cf54f3f7c34bb7b633ab249cec27df523f68c0
charlenebatiste <batistecharlene@gmail.com>
2021-06-14 17:00:36 -0400

    fixed hero styling

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,18 +4,21 @@ import "./css/Hero.css";
 const Hero = () => {
 	return (
 		<div className="jumbotron">
-			<div className="container-sm">
-				<h1 className="display-4">
-					Health, made tasty.{" "}
-				</h1>
-				<p className="body">
-					Welcome to Healthful Pantry
-					and start your journey to
-					health freedom today!
-				</p>
-
-				{/* <p className="body">Eat your way back to health and fight back from chronic illness and disease. </p> */}
-				<p className="lead">
+			<div className="container-fluid">
+				<div className="row">
+					<h1 className="hero__header mx-5">
+						Health, made tasty.
+					</h1>
+				</div>
+				<div className="row">
+					<p className="col-sm-6 hero__content mx-5">
+						Welcome to Healthful
+						Pantry and start your
+						journey to health freedom
+						today!
+					</p>
+				</div>
+				<p className="lead mx-5">
 					<a
 						className="btn btn-primary btn-lg"
 						href="/search"
