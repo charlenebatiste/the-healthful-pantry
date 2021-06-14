c14f92a91acd105005b6c32071926b4cace869be
charlenebatiste <batistecharlene@gmail.com>
2021-06-14 15:37:37 -0400

    added icons to how it works component

--- src/components/HowItWorks.jsx
+++ src/components/HowItWorks.jsx
@@ -1,5 +1,9 @@
 import React from "react";
 import "./css/HowItWorks.css";
+import SearchIcon from "../img/search-icon.png";
+import FavoriteIcon from "../img/favorites-icon.png";
+import CartIcon from "../img/cart-icon.png";
+
 import {
 	Grid,
 	Card,
@@ -7,22 +11,34 @@ import {
 	CardContent,
 } from "@material-ui/core";
 
-
 const Steps = () => {
 	return (
-		<div className="step_section">
-			<h1 className="text-center hIW">
+		<div className="step_section py-4">
+			<h1 className="text-center">
 				How It Works
 			</h1>
-			<Grid container alignItems="stretch">
+
+			<Grid
+				container
+				className="gridContainer"
+			>
 				<Grid
 					item
 					className="cardItem"
 					component={Card}
 					xs
 				>
-					<CardContent>
+					<CardContent className="cardContent">
+						<div className="img__container p-2">
+							<img
+								className="icon-img"
+								width="25%"
+								height="25%"
+								src={SearchIcon}
+							/>
+						</div>
 						<Typography
+							className="cardText"
 							gutterBottom
 							variant="h5"
 							component="h2"
@@ -39,15 +55,23 @@ const Steps = () => {
 						</Typography>
 					</CardContent>
 				</Grid>
-
 				<Grid
 					item
 					className="cardItem"
 					component={Card}
 					xs
 				>
-					<CardContent>
+					<CardContent className="cardContent">
+						<div className="img__container p-2">
+							<img
+								className="icon-img"
+								width="25%"
+								height="25%"
+								src={FavoriteIcon}
+							/>
+						</div>
 						<Typography
+							className="cardText"
 							gutterBottom
 							variant="h5"
 							component="h2"
@@ -64,15 +88,23 @@ const Steps = () => {
 						</Typography>
 					</CardContent>
 				</Grid>
-
 				<Grid
 					item
 					className="cardItem"
 					component={Card}
 					xs
 				>
-					<CardContent>
+					<CardContent className="cardContent">
+						<div className="img__container p-2">
+							<img
+								className="icon-img"
+								width="25%"
+								height="25%"
+								src={CartIcon}
+							/>
+						</div>
 						<Typography
+							className="cardText"
 							gutterBottom
 							variant="h5"
 							component="h2"
@@ -84,9 +116,10 @@ const Steps = () => {
 							color="textSecondary"
 							component="p"
 						>
-							Print your shopping
-							list and eat your way
-							to health.
+							Generate a Shopping
+							List to make sure you
+							have everything you
+							need
 						</Typography>
 					</CardContent>
 				</Grid>
